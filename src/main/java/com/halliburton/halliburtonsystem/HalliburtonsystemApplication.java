955e4518c05c607393fb5a8e18724177cf662536
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-08-18 09:29:58 -0300

    creating project and adding security settings

---
+++ src/main/java/com/halliburton/halliburtonsystem/HalliburtonsystemApplication.java
@@ -0,0 +1,13 @@
+package com.halliburton.halliburtonsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalliburtonsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalliburtonsystemApplication.class, args);
+	}
+
+}
